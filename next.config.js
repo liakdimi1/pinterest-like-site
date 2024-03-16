a6e1e1102cb71ea30bf284be738642b7f596346c
Dimitris Liakos <105444235+liakdimi1@users.noreply.github.com>
2024-03-16 11:04:15 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "platform-lookaside.fbsbx.com",
+      "lh3.googleusercontent.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
